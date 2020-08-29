aae71fea194265045d1da6b46a62d0ac4659cbb5
madhuvayya <madhu.vayya@gmail.com>
2020-08-29 23:26:22 +0530

    Refactored code to follow single responsibility principle

--- UserManagementSystem/src/com/bridgelabz/usermanagementsystem/controller/Login.java
+++ UserManagementSystem/src/com/bridgelabz/usermanagementsystem/controller/Login.java
@@ -2,51 +2,43 @@ package com.bridgelabz.usermanagementsystem.controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.ResultSet;
-import java.sql.Connection;
 import javax.servlet.http.HttpSession;
-import java.sql.PreparedStatement;
-import com.bridgelabz.usermanagementsystem.config.*;
+import com.bridgelabz.usermanagementsystem.dao.UserDao;
+import com.bridgelabz.usermanagementsystem.model.User;
 
 @WebServlet(value = "/login-process")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		String userName = request.getParameter("username");
 		String password = request.getParameter("password");
 		String destinationPath = "";
 
-		Connection connection = null;
-		try {
-			connection = DBConnection.getConnection();
-			PreparedStatement preparedStatement = connection.prepareStatement(
-					"select * from user_info where user_name ='" + userName + "' and password='" + password + "'");
+		User user = new User();
+		user.setUserName(userName);
+		user.setPassword(password);
 
-			ResultSet resultSet = preparedStatement.executeQuery();
-
-			if (!resultSet.next()) {
-				request.setAttribute("message", "Enter registred user name and password.");
-				destinationPath = "login";
-			} else {
-				destinationPath = "dashboard";
-
-				HttpSession httpSession = request.getSession();
-				httpSession.setAttribute("username", userName);
-				httpSession.setAttribute("password", password);
-			}
-
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher(destinationPath);
-			connection.close();
-			requestDispatcher.forward(request, response);
-		} catch (Exception exception) {
-			exception.printStackTrace();
+		UserDao userDao = new UserDao();
+		boolean isAuthorizedUser = userDao.checkUserAuthorization(user);
+		
+		if (isAuthorizedUser) {
+			destinationPath = "dashboard";
+			
+			HttpSession httpSession = request.getSession();
+			httpSession.setAttribute("username", userName);
+			httpSession.setAttribute("password", password);			
+		} else {
+			request.setAttribute("message", "Enter registred user name and password.");
+			destinationPath = "login";
 		}
+		request.getRequestDispatcher(destinationPath).forward(request, response);
 	}
 }
--- UserManagementSystem/src/com/bridgelabz/usermanagementsystem/dao/UserDao.java
+++ UserManagementSystem/src/com/bridgelabz/usermanagementsystem/dao/UserDao.java
@@ -2,11 +2,40 @@ package com.bridgelabz.usermanagementsystem.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.bridgelabz.usermanagementsystem.config.DBConnection;
 import com.bridgelabz.usermanagementsystem.model.User;
 
 public class UserDao {
+	
+	public boolean checkUserAuthorization(User user) {
+		String userName = user.getUserName();
+		String password = user.getPassword();
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			connection = DBConnection.getConnection();
+			preparedStatement = connection.prepareStatement(
+					"select * from user_info where user_name ='" + userName + "' and password='" + password + "'");
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+			return resultSet.next();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}finally {
+			if (connection != null)
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		return false;		
+	}
+		
 	public boolean registerUser(User user) {
 		String registerQuery = "INSERT INTO user_info (first_name,middle_name,last_name,dob,gender, email, country,phone_number,alternate_number,address,user_name,password,role) "
 				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
@@ -30,12 +59,17 @@ public class UserDao {
 			preparedStatement.setString(11, user.getUserName());
 			preparedStatement.setString(12, user.getPassword());
 			preparedStatement.setString(13, user.getRole());
-
 			return preparedStatement.executeUpdate() != 0;
 		} catch (Exception exception) {
 			exception.printStackTrace();
+		}finally {
+			if (connection != null)
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 		}
-
 		return false;
 	}
 }
